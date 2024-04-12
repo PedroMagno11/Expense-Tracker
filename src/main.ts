6c512c30c70544356e50feb82dbe2beec981ffac
Pedro Magno <pedromagnopro@gmail.com>
2024-04-11 23:49:53 -0300

    Init: configurações iniciais do projeto

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import Toast from 'vue-toastification';
+
+import 'vue-toastification/dist/index.css';
+import './assets/style.css';
+
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(Toast);
+app.use(router);
+
+app.mount('#app');
